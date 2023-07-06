e180377c8bcdf429b0edfc25d3140acdfcecbfdc
Sirothpech <christophe.ngan@gmail.com>
2023-07-06 11:18:48 +0200

    Add: city.py create Public class attributes state_id: string - empty string: it will be the State.id name: string - empty string

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains the City class inherit from the BaseModel class.
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """City class"""
+    state_id = ""
+    name = ""
