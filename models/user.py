a63d8d8eb09643ba36608bc0218cb9c54fd82618
Sirothpech <christophe.ngan@gmail.com>
2023-07-06 11:22:32 +0200

    Add: user.py Write a class User that inherits from BaseModel

---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module contains the User class inherit from the BaseModel class.
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """User class that inherits from BaseModel"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
