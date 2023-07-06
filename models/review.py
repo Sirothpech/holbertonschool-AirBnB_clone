a5def73cc31bfb3d21cf2407f53c46c4a20d1d43
Sirothpech <christophe.ngan@gmail.com>
2023-07-06 11:20:55 +0200

    Add review.py create Public class attributes: place, user, text

---
+++ models/review.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module contains the Review class inherit from the BaseModel class.
+"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Review class"""
+    place_id = ""
+    user_id = ""
+    text = ""
