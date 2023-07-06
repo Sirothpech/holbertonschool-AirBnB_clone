ff918f85ec9b1e1d7dcbc56b31edd528780aa503
MarcP70 <marcpourias@yahoo.fr>
2023-07-06 10:49:31 +0200

    Add: file test_base_model.py for unit tests the file base_model.py

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import unittest
+from models.base_model import BaseModel
+
+class TestBase_model_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the Base_model class."""
+
+if __name__ == "__main__":
+    unittest.main()
