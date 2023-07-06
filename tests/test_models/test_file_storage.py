791efef91a10e6d57384cf2fb008cd05af5782f8
Sirothpech <christophe.ngan@gmail.com>
2023-07-06 14:37:59 +0200

    Update: test_file_storage.py unittests

--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -6,9 +6,16 @@ Unittests for the FileStorage class
 import unittest
 import os
 import json
+from datetime import datetime
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models.engine.file_storage import FileStorage
+
 
 class TestFileStorage(unittest.TestCase):
     """Test cases for the FileStorage class"""
@@ -43,16 +50,15 @@ class TestFileStorage(unittest.TestCase):
         """Test the new() method"""
         user = User()
         user.id = "12345"
-        user.save()
-
-        all_objects = self.storage.all()
-        self.assertIn("User.12345", all_objects)
+        self.storage.new(user)
+        self.assertIn("User.12345", self.storage._FileStorage__objects)
 
     def test_save_method(self):
         """Test the save() method"""
         user = User()
         user.id = "12345"
-        user.save()
+        self.storage.new(user)
+        self.storage.save()
 
         file_path = FileStorage._FileStorage__file_path
         self.assertTrue(os.path.isfile(file_path))
@@ -65,23 +71,29 @@ class TestFileStorage(unittest.TestCase):
         """Test the reload() method"""
         user = User()
         user.id = "12345"
-        user.save()
-
-        file_path = FileStorage._FileStorage__file_path
-
-        # Modify the content of the JSON file
-        with open(file_path, 'r+') as file:
-            data = json.load(file)
-            data["FakeObject"] = {"id": "54321"}
-            file.seek(0)
-            json.dump(data, file)
-            file.truncate()
+        self.storage.new(user)
+        self.storage.save()
 
         self.storage.reload()
 
         all_objects = self.storage.all()
         self.assertIn("User.12345", all_objects)
-        self.assertNotIn("FakeObject.54321", all_objects)
+
+    def test_reload_with_datetime_attribute(self):
+        """Test the reload() method with datetime attribute"""
+        current_time = datetime.now()
+        base_model = BaseModel()
+        base_model.id = "12345"
+        base_model.created_at = current_time
+        self.storage.new(base_model)
+        self.storage.save()
+
+        self.storage.reload()
+
+        all_objects = self.storage.all()
+        self.assertIn("BaseModel.12345", all_objects)
+        self.assertIsInstance(all_objects["BaseModel.12345"].created_at, datetime)
+        self.assertEqual(all_objects["BaseModel.12345"].created_at, current_time)
 
 if __name__ == '__main__':
     unittest.main()
