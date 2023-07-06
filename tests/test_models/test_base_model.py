bb0342dd68a0b188eb95dd63a47a74f6c725fb14
Sirothpech <christophe.ngan@gmail.com>
2023-07-06 14:17:48 +0200

    Update: test_base_model.to add some py unittests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,9 +6,39 @@ import unittest
 from models.base_model import BaseModel
 
 
-class TestBase_model_instantiation(unittest.TestCase):
+class TestBaseModel(unittest.TestCase):
     """Unittests for testing instantiation of the Base_model class."""
+    def setUp(self):
+        self.model = BaseModel()
 
+    def test_attributes(self):
+        self.assertTrue(hasattr(self.model, 'id'))
+        self.assertTrue(hasattr(self.model, 'created_at'))
+        self.assertTrue(hasattr(self.model, 'updated_at'))
+
+    def test_save_method(self):
+        prev_updated_at = self.model.updated_at
+        self.model.save()
+        self.assertNotEqual(prev_updated_at, self.model.updated_at)
+
+    def test_to_dict_method(self):
+        model_dict = self.model.to_dict()
+        self.assertEqual(model_dict['__class__'], 'BaseModel')
+        self.assertEqual(model_dict['id'], self.model.id)
+        self.assertEqual(model_dict['created_at'], self.model.created_at.isoformat())
+        self.assertEqual(model_dict['updated_at'], self.model.updated_at.isoformat())
+
+    def test_from_dict_method(self):
+        model_dict = {
+            '__class__': 'BaseModel',
+            'id': '12345',
+            'created_at': '2022-01-01T12:00:00.000000',
+            'updated_at': '2022-01-01T12:00:00.000000'
+        }
+        new_model = BaseModel(**model_dict)
+        self.assertEqual(new_model.id, '12345')
+        self.assertEqual(new_model.created_at.isoformat(), '2022-01-01T12:00:00')
+        self.assertEqual(new_model.updated_at.isoformat(), '2022-01-01T12:00:00')
 
 if __name__ == "__main__":
     unittest.main()
