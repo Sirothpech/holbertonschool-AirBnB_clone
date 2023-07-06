5940a21eca2ed4de0a406fffe9d6f3116cfb6373
MarcP70 <marcpourias@yahoo.fr>
2023-07-06 10:50:20 +0200

    Add: file test_user.py for unit tests the file user.py

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import unittest
+from models.user import User
+
+class TestUser_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the User class."""
+
+if __name__ == "__main__":
+    unittest.main()
